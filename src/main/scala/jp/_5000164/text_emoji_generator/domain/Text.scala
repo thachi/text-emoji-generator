977962da1ab783a89ce77f3c53bc17fdc71e2713
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2018-05-09 15:48:00 +0900

    PrintCharを作る処理をtoPrintCharとしてalignに応じて求めるようにしました。

--- src/main/scala/jp/_5000164/text_emoji_generator/domain/Text.scala
+++ src/main/scala/jp/_5000164/text_emoji_generator/domain/Text.scala
@@ -8,47 +8,45 @@ object Text {
   }
 
   def calculatePosition(lines: List[String], align: Align): List[PrintChar] = {
-    val side = 128
-    val maxLength = lines.map(_.length).max
-    val maxRow = lines.length
-    val maxWidth = side / Math.max(maxLength, maxRow)
 
-    val heightUnit = side / maxRow
-    val heightUnitCenter = heightUnit / 2
-    val widthUnit = side / maxLength
-    val widthUnitCenter = widthUnit / 2
+    val toPrintChar = {
+      val side = 128
+      val maxLength = lines.map(_.length).max
+      val maxRow = lines.length
+      val maxWidth = side / Math.max(maxLength, maxRow)
 
-    val toPrintCharForAlignLeft = (char: Char, rowIndex: Int, columnIndex: Int) => PrintChar(
-      char.toString,
-      widthUnit * columnIndex + widthUnitCenter,
-      heightUnit * rowIndex + heightUnitCenter,
-      maxWidth
-    )
+      val heightUnit = side / maxRow
+      val heightUnitCenter = heightUnit / 2
+      val widthUnit = side / maxLength
+      val widthUnitCenter = widthUnit / 2
 
-    val toPrintCharForAlignCenter = (lineLength: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
-      val lineWidth = widthUnit * lineLength
-      val margin = (side - lineWidth) / 2
-      PrintChar(
+      val toPrintCharForAlignLeft = (_: Int, char: Char, rowIndex: Int, columnIndex: Int) => PrintChar(
         char.toString,
-        margin + widthUnit * columnIndex + widthUnitCenter,
+        widthUnit * columnIndex + widthUnitCenter,
         heightUnit * rowIndex + heightUnitCenter,
         maxWidth
       )
+
+      val toPrintCharForAlignCenter = (lineLength: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
+        val lineWidth = widthUnit * lineLength
+        val margin = (side - lineWidth) / 2
+        PrintChar(
+          char.toString,
+          margin + widthUnit * columnIndex + widthUnitCenter,
+          heightUnit * rowIndex + heightUnitCenter,
+          maxWidth
+        )
+      }
+      align match {
+        case Left => toPrintCharForAlignLeft
+        case Center => toPrintCharForAlignCenter
+      }
     }
 
-    align match {
-      case Left =>
-        for {
-          (line, rowIndex) <- lines.zipWithIndex
-          (char, columnIndex) <- line.zipWithIndex
-        } yield toPrintCharForAlignLeft(char, rowIndex, columnIndex)
-
-      case Center =>
-        for {
-          (line, rowIndex) <- lines.zipWithIndex
-          (char, columnIndex) <- line.zipWithIndex
-        } yield toPrintCharForAlignCenter(line.length, char, rowIndex, columnIndex)
-    }
+    for {
+      (line, rowIndex) <- lines.zipWithIndex
+      (char, columnIndex) <- line.zipWithIndex
+    } yield toPrintChar(line.length, char, rowIndex, columnIndex)
   }
 }
 
