6b8b9601cfb36858107cd33441d3aa18ef9c2412
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2018-05-09 16:02:23 +0900

    試しにtoPrintCharを生成するメソッドを抜き出してみました。 この変更は保守性が上がるかはちょっと疑問です。 右添えの処理は追加しやすそうです。

--- src/main/scala/jp/_5000164/text_emoji_generator/domain/Text.scala
+++ src/main/scala/jp/_5000164/text_emoji_generator/domain/Text.scala
@@ -9,46 +9,48 @@ object Text {
 
   def calculatePosition(lines: List[String], align: Align): List[PrintChar] = {
 
-    val toPrintChar = {
-      val side = 128
-      val maxLength = lines.map(_.length).max
-      val maxRow = lines.length
-      val maxWidth = side / Math.max(maxLength, maxRow)
-
-      val heightUnit = side / maxRow
-      val heightUnitCenter = heightUnit / 2
-      val widthUnit = side / maxLength
-      val widthUnitCenter = widthUnit / 2
-
-
-      def toPrintChar(margin: Int, char: Char, rowIndex: Int, columnIndex: Int) = {
-        PrintChar(
-          char.toString,
-          margin + widthUnit * columnIndex + widthUnitCenter,
-          heightUnit * rowIndex + heightUnitCenter,
-          maxWidth
-        )
-      }
-
-      val toPrintCharForAlignLeft = (_: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
-        toPrintChar(0, char, rowIndex, columnIndex)
-      }
-
-      val toPrintCharForAlignCenter = (lineLength: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
-        val lineWidth = widthUnit * lineLength
-        val margin = (side - lineWidth) / 2
-        toPrintChar(margin, char, rowIndex, columnIndex)
-      }
-      align match {
-        case Left => toPrintCharForAlignLeft
-        case Center => toPrintCharForAlignCenter
-      }
-    }
+    val toPrintChar = createToPrintChar(align, lines.map(_.length).max, lines.length)
 
     for {
       (line, rowIndex) <- lines.zipWithIndex
       (char, columnIndex) <- line.zipWithIndex
     } yield toPrintChar(line.length, char, rowIndex, columnIndex)
+  }
+
+  /**
+    * PrintCharを作る関数を生成します。
+    *
+    * @param align Align
+    * @param maxLength 最大の文字数
+    * @param maxRow 最大の行数
+    * @return 行の文字数、文字、行インデックス,列インデックスに応じてPrintCharを生成するための関数を返します。
+    */
+  private def createToPrintChar(align: Align, maxLength: Int, maxRow: Int) = {
+    val side = 128
+    val maxWidth = side / Math.max(maxLength, maxRow)
+
+    val heightUnit = side / maxRow
+    val heightUnitCenter = heightUnit / 2
+    val widthUnit = side / maxLength
+    val widthUnitCenter = widthUnit / 2
+
+    def toPrintChar(margin: Int, char: Char, rowIndex: Int, columnIndex: Int) = PrintChar(
+      char.toString,
+      margin + widthUnit * columnIndex + widthUnitCenter,
+      heightUnit * rowIndex + heightUnitCenter,
+      maxWidth
+    )
+
+    align match {
+      case Left => (_: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
+        toPrintChar(0, char, rowIndex, columnIndex)
+      }
+      case Center => (lineLength: Int, char: Char, rowIndex: Int, columnIndex: Int) => {
+        val lineWidth = widthUnit * lineLength
+        val margin = (side - lineWidth) / 2
+        toPrintChar(margin, char, rowIndex, columnIndex)
+      }
+    }
   }
 }
 
